dad2906f92cf35d8d731b47efff0a5d5ba194096
kylehui <o1093001@gm.pu.edu.tw>
2022-03-11 13:43:15 +0800

    第一次 commit

---
+++ app/src/main/java/tw/edu/pu/gm/o1093001/hellokyle/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.gm.o1093001.hellokyle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
